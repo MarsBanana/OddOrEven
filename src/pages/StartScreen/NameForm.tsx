72ff280a0122e350b80b78859c667f05e0275145
marsbanana <nikita@thescholasticnetwork.com>
2020-12-15 19:49:05 +0700

    name registers onSubmit

--- src/pages/StartScreen/NameForm.tsx
+++ src/pages/StartScreen/NameForm.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react"
+import React, {useState, useRef} from "react"
 import {Button, List, ListInput, Popup} from "framework7-react"
 
 const popupCustomStyle = {
@@ -9,14 +9,25 @@ const popupCustomStyle = {
 const NameForm: React.FC = () => {
     const [isNameFormOpen, setIsNameFormOpen] = useState<boolean>(true)
 
+    const nameRef = useRef<any>(null)
+
     const handleConfirm = () => {
         setIsNameFormOpen(false)
+        const name = nameRef.current.__reactRefs.inputEl.value
+        console.log(name)
     }
 
     return (
         <Popup closeByBackdropClick={false} opened={isNameFormOpen} style={popupCustomStyle}>
             <List inlineLabels>
-                <ListInput type="text" label="Name" placeholder="Your name" autofocus clearButton />
+                <ListInput
+                    ref={nameRef}
+                    type="text"
+                    label="Name"
+                    placeholder="Your name"
+                    autofocus
+                    clearButton
+                />
             </List>
             <Button onClick={handleConfirm}>Confirm Name</Button>
         </Popup>
