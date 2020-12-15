16f810fba37d4b5ae751c6f61319bed690a5bc57
marsbanana <nikita@thescholasticnetwork.com>
2020-12-15 19:04:27 +0700

    added name form on start page

--- src/pages/StartScreen/index.tsx
+++ src/pages/StartScreen/index.tsx
@@ -1,5 +1,19 @@
+import {Button, List, ListInput, Popup} from "framework7-react"
+
+const popupCustomStyle = {
+    maxHeight: "180px",
+    marginTop: "-120px",
+}
+
 const StartScreen = () => {
-    return null
+    return (
+        <Popup closeByBackdropClick={false} opened={true} style={popupCustomStyle}>
+            <List inlineLabels>
+                <ListInput type="text" label="Name" placeholder="Your name" autofocus clearButton />
+            </List>
+            <Button>Confirm Name</Button>
+        </Popup>
+    )
 }
 
 export default StartScreen
