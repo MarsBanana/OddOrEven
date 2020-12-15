07071d05a587829487783c04ecddb96ec887483e
marsbanana <nikita@thescholasticnetwork.com>
2020-12-16 01:43:27 +0700

    added buttons

--- src/pages/StartScreen/index.tsx
+++ src/pages/StartScreen/index.tsx
@@ -1,8 +1,25 @@
+import {Block, Button, Row} from "framework7-react"
 import React from "react"
 import NameForm from "./NameForm"
 
+const customRowStyle = {
+    maxWidth: "300px",
+    marginTop: "40vh",
+    marginLeft: "calc(50% - 150px)",
+}
+
 const StartScreen: React.FC = () => {
-    return <NameForm />
+    return (
+        <>
+            <Block>
+                <Row style={customRowStyle}>
+                    <Button>Join</Button>
+                    <Button>Create</Button>
+                </Row>
+            </Block>
+            <NameForm />
+        </>
+    )
 }
 
 export default StartScreen
