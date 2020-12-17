326ba3baa388f035146eecbf80c7199bcd218765
marsbanana <nikita@thescholasticnetwork.com>
2020-12-17 23:17:55 +0700

    removed clearButton

--- src/pages/CreateScreen/CreateGameForm.tsx
+++ src/pages/CreateScreen/CreateGameForm.tsx
@@ -40,7 +40,6 @@ const CreateGameForm: React.FC = () => {
                     type="text"
                     label="Name your game"
                     placeholder="Name"
-                    clearButton
                 />
                 <Block>
                     <BlockTitle style={blockTitleCustomStyle}>Amount of players</BlockTitle>
