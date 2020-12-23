3dd6bee43106d56e0889beb4e522c3f4b9d8f685
marsbanana <nikita@thescholasticnetwork.com>
2020-12-23 20:53:05 +0700

    exported GoBack

--- src/pages/JoinScreen/index.tsx
+++ src/pages/JoinScreen/index.tsx
@@ -1,8 +1,13 @@
 import {Page} from "framework7-react"
+import GoBack from "../../components/GoBack"
 import React from "react"
 
 const JoinScreen: React.FC = () => {
-    return <Page></Page>
+    return (
+        <Page>
+            <GoBack />
+        </Page>
+    )
 }
 
 export default JoinScreen
