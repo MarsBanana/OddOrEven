4a9f7f82f65fd37b6e63d6bd76e5626d4051e355
marsbanana <nikita@thescholasticnetwork.com>
2020-12-26 17:59:21 +0700

    add page skeleton

--- src/pages/JoinScreen/index.tsx
+++ src/pages/JoinScreen/index.tsx
@@ -1,11 +1,25 @@
-import {Page} from "framework7-react"
+import {Block, Button, Page} from "framework7-react"
 import GoBack from "../../components/GoBack"
-import React from "react"
+import React, {useEffect} from "react"
+import {useDispatch} from "react-redux"
+import {fetchGamesList} from "../../store/actions"
+
+const customBlockStyle = {
+    marginTop: "20vh",
+}
 
 const JoinScreen: React.FC = () => {
+    const dispatch = useDispatch()
+    const fetch = () => dispatch(fetchGamesList())
+    useEffect(() => {
+        fetch()
+    })
     return (
         <Page>
-            <GoBack />
+            <Block style={customBlockStyle}>
+                <Button onClick={fetch}>Update</Button>
+                <GoBack />
+            </Block>
         </Page>
     )
 }
