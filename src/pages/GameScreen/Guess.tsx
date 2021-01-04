07c26c05a6ca902e0e673623676873b35041ecba
marsbanana <nikita@thescholasticnetwork.com>
2021-01-05 00:19:15 +0700

    guess interface

--- src/pages/GameScreen/Guess.tsx
+++ src/pages/GameScreen/Guess.tsx
@@ -1,7 +1,17 @@
 import React from "react"
+import {Row, Button} from "framework7-react"
+import CenteredText from "../../components/CenteredText"
 
 const Guess: React.FC = () => {
-    return <></>
+    return (
+        <>
+            <CenteredText title text="Choose wisely :)" />
+            <Row>
+                <Button>ODD</Button>
+                <Button>EVEN</Button>
+            </Row>
+        </>
+    )
 }
 
 export default Guess
