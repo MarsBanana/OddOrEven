ddcb6f7517744b3ca4023aa53c05262b11f6693f
marsbanana <nikita@thescholasticnetwork.com>
2021-01-05 21:09:40 +0700

    decompose GameInfo

--- src/pages/GameScreen/GameInfo.tsx
+++ src/pages/GameScreen/GameInfo.tsx
@@ -1,7 +1,27 @@
 import React from "react"
 import CenteredText from "../../components/CenteredText"
 import {List, ListItem} from "framework7-react"
-import {GameData, phaseTypes} from "../../store/types"
+import {GameData, MoveData, phaseTypes, Player} from "../../store/types"
+
+const Phase: React.FC<{currentMove?: MoveData}> = ({currentMove}) => (
+    <CenteredText
+        text={`${currentMove?.name} is ${
+            currentMove?.phase === phaseTypes.GUESS ? "guessing" : "picking"
+        }`}
+    />
+)
+
+const RoundAnnouncer: React.FC<{roundsLeft: number}> = ({roundsLeft}) => (
+    <CenteredText text={roundsLeft !== 1 ? `${roundsLeft} rounds left` : `Final round`} />
+)
+
+const PlayersList: React.FC<{players: Array<Player>}> = ({players}) => (
+    <List>
+        {players.map((player, idx) => (
+            <ListItem key={player.name + idx} after={`${player.points}`} title={player.name} />
+        ))}
+    </List>
+)
 
 const GameInfo: React.FC<{data: GameData}> = ({data}) => (
     <>
@@ -12,21 +32,11 @@ const GameInfo: React.FC<{data: GameData}> = ({data}) => (
             />
         ) : (
             <>
-                <CenteredText
-                    text={`${data.currentMove?.name} is ${
-                        data.currentMove?.phase === phaseTypes.GUESS ? "guessing" : "picking"
-                    }`}
-                />
-                <CenteredText
-                    text={data.roundsLeft !== 1 ? `${data.roundsLeft} rounds left` : `Final round`}
-                />
+                <Phase currentMove={data.currentMove} />
+                <RoundAnnouncer roundsLeft={data.roundsLeft} />
             </>
         )}
-        <List>
-            {data.players.map((player, idx) => (
-                <ListItem key={player.name + idx} after={`${player.points}`} title={player.name} />
-            ))}
-        </List>
+        <PlayersList players={data.players} />
     </>
 )
 
