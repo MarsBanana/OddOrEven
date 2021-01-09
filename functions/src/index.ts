674ebb3f9b68a7177c7a0e5989a4f391a91d891c
marsbanana <nikita@thescholasticnetwork.com>
2021-01-09 21:47:04 +0700

    fix function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,22 +1,27 @@
 import * as functions from 'firebase-functions';
 import * as admin from "firebase-admin"
+import collections from './collections';
 
 admin.initializeApp()
 
 const db = admin.firestore()
 
 exports.deleteGameDoc = functions.firestore
-  .document('GAMES_LIST/{gameId}')
+  .document(`${collections.GAMES_LIST}/{gameId}`)
   .onUpdate((change) => {
+
       const gameData = change.after.data()
       const gameId = change.after.id
       
       const playersAmount = gameData.players.length
 
       if (playersAmount === 0) {
-        db.collection("GAMES_LIST").doc(gameId)
+        return db.collection(collections.GAMES_LIST).doc(gameId)
           .delete()
-          .catch((e) => {console.log(e)})
+          .then(() => {console.log("Deleted game doc with id = " + gameId)})
+          .catch((e) => {console.log("Failed deleteGameDoc", e)})
+      } else {
+        return console.log(gameId + " is still going")
       }
 
   })
